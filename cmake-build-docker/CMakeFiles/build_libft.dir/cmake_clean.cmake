97abc8de884f348f26ed592a793b5ea72d304a3e
Yelyzaveta Berezhna <yberezhn@student.42london.com>
2025-02-19 16:17:13 +0000

    removed unused files from git

--- cmake-build-docker/CMakeFiles/build_libft.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/build_libft"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/build_libft.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
